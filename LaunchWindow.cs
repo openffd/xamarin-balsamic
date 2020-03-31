84ad1bbead51e6e69698234de6b43f9720a8e1b4
ffd <ffdelacruz@icloud.com>
2020-03-31 19:45:52 +0800

    Add aesthetic changes to LaunchWindow

--- LaunchWindow.cs
+++ LaunchWindow.cs
@@ -8,10 +8,13 @@ namespace Balsamic
         public LaunchWindow(CoreGraphics.CGRect contentRect, NSWindowStyle aStyle, NSBackingStore bufferingType, bool deferCreation) :
             base(contentRect, aStyle, bufferingType, deferCreation)
         {
+            Appearance = NSAppearance.GetAppearance(NSAppearance.NameVibrantDark);
             IsOpaque = true;
-            MovableByWindowBackground = false;
+            MovableByWindowBackground = true;
             ReleasedWhenClosed = false;
             Title = NSBundle.MainBundle.Name();
+            TitlebarAppearsTransparent = true;
+            TitleVisibility = NSWindowTitleVisibility.HiddenWhenActive;
         }
 
         public override void AwakeFromNib()
