41dd0d0c0fca85748034052ad21130e0361ed7ee
ffd <ffdelacruz@icloud.com>
2020-04-02 00:16:04 +0800

    Add static class StaticNSUrl

---
+++ Statics/StaticNSUrl.cs
@@ -0,0 +1,13 @@
+﻿namespace Balsamic
+{
+    public static class NSUrl
+    {
+        private static class String
+        {
+            public const string ForgotPassword = "iforgot.apple.com/password/verify/appleid";
+            public const string HttpsScheme = "https";
+        }
+        
+        public static Foundation.NSUrl ForgotPassword = new Foundation.NSUrl($"{String.HttpsScheme}://{String.ForgotPassword}");
+    }
+}
