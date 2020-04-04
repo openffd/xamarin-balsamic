7eb040ab5cf0483b8fbdf47db561050001233105
ffd <ffdelacruz@icloud.com>
2020-04-04 15:36:08 +0800

    Create String and Convertible static classes for string constants

---
+++ Statics/Convertible.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+
+namespace Balsamic
+{
+    public static class Convertible
+    {
+        public static string GetDescription<T>(this T e) where T : IConvertible
+        {
+            if (e is Enum)
+            {
+                Type type = e.GetType();
+                Array values = System.Enum.GetValues(type);
+
+                foreach (int val in values)
+                {
+                    if (val == e.ToInt32(CultureInfo.InvariantCulture))
+                    {
+                        var memInfo = type.GetMember(type.GetEnumName(val));
+                        var descriptionAttribute = memInfo[0]
+                            .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                            .FirstOrDefault() as DescriptionAttribute;
+
+                        if (descriptionAttribute != null)
+                        {
+                            return descriptionAttribute.Description;
+                        }
+                    }
+                }
+            }
+
+            return null; // could also return string.Empty
+        }
+  return description;
+}
+
+
+    public static string String<T>(this T someEnum) where T : IConvertible
+        {
+            if (!(someEnum is Enum))
+            {
+                return string.Empty;
+            }
+
+            Type type = someEnum.GetType();
+            Array array = Enum.GetValues(type);
+            foreach (int item in array)
+            {
+                if (item != someEnum.ToInt32(CultureInfo.InvariantCulture))
+                {
+                    continue;
+                }
+
+                if (type.GetMember(type.GetEnumName(item))[0]
+                    .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                    .FirstOrDefault() is DescriptionAttribute descriptionAttribute)
+                {
+                    return descriptionAttribute.Description;
+                }
+            }
+        }
+    }
+}
---
+++ Statics/String.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Balsamic
+{
+    public static class String
+    {
+        public enum ErrorMessage
+        {
+            [Description("⚠️ Not a valid email")]
+            InvalidEmail = 0
+        }
+    }
+}
