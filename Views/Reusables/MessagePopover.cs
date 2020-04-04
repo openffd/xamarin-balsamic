6f66406292be482e652f5dda30e655223791ea2e
ffd <ffdelacruz@icloud.com>
2020-04-04 17:30:35 +0800

    Add reusable MessagePopover

---
+++ Views/Reusables/MessagePopover.cs
@@ -0,0 +1,25 @@
+﻿using AppKit;
+
+namespace Balsamic.Views
+{
+    public class MessagePopover : NSPopover
+    {
+        private string _message = "";
+        public string Message
+        {
+            get => _message;
+            set {
+                _message = value;
+                ((MessageViewController)ContentViewController).Message = value;
+            }
+        }
+
+        public MessagePopover(string message = "")
+        {
+            _message = message;
+            Animates = true;
+            Behavior = NSPopoverBehavior.Transient;
+            ContentViewController = new MessageViewController(message);
+        }
+    }
+}
