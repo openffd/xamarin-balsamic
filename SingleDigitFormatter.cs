d7e3a234a34c4ef435163a3551580ed2a6fc5704
ffd <ffdelacruz@icloud.com>
2020-04-07 22:43:03 +0800

    SingleDigitFormatter: Implement overridable methods from NSFormatter

--- SingleDigitFormatter.cs
+++ SingleDigitFormatter.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Foundation;
+
 namespace Balsamic
 {
-    public class SingleDigitFormatter
+    public class SingleDigitFormatter : NSFormatter
     {
-        public SingleDigitFormatter()
+        public SingleDigitFormatter() {}
+
+        public override string StringFor(NSObject value) => value.ToString();
+
+        public override bool GetObjectValue(out NSObject obj, string str, out NSString error)
         {
+            obj = (NSString)str;
+            error = null;
+            return true;
+        }
+
+        public override bool IsPartialStringValid(
+            ref string partialString,
+            out NSRange proposedSelRange,
+            string origString,
+            NSRange origSelRange,
+            out string error)
+        {
+            error = null;
+            proposedSelRange = new NSRange(0, 1);
+            switch (partialString.Length)
+            {
+                case 0:
+                    partialString = string.Empty;
+                    return false;
+                case 1:
+                    return int.TryParse(partialString, out _);
+                default:
+                    partialString = partialString.Substring(partialString.Length - 1);
+                    return false;
+            }
         }
     }
 }
