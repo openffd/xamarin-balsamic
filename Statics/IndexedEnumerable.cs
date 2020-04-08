2732a63793386e850d3817cbf6e557ecce64465f
ffd <ffdelacruz@icloud.com>
2020-04-08 22:49:12 +0800

    Create IndexedEnumerable static class

---
+++ Statics/IndexedEnumerable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Balsamic
+{
+    public static class IndexedEnumerable
+    {
+        public static IEnumerable<(T item, int index)> Indexed<T>(this IEnumerable<T> self)
+        {
+            return self.Select((item, index) => (item, index));
+        }
+    }
+}
