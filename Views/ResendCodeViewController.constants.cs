9f4a597e4ac8b50281910e0a5230fb05891c8374
ffd <ffdelacruz@icloud.com>
2020-04-12 16:34:40 +0800

    Add ResendCodeVC.constants partial class to contain related constants

---
+++ Views/ResendCodeViewController.constants.cs
@@ -0,0 +1,45 @@
+﻿using AppKit;
+
+namespace Balsamic.Views
+{
+    sealed partial class ResendCodeViewController
+    {
+       static class String
+        {
+            internal static class ResendCode
+            {
+                internal static string Header => "Resend Code";
+                internal static string Description => "Get a new verification code";
+                internal static string ImageName => "NSRefreshTemplate";
+            }
+
+            internal static class UsePhoneNumber
+            {
+                internal static string Header => "Use Phone Number";
+                internal static string Description => "Get a text or phone call with a code";
+                internal static string ImageName => "NSTouchBarNewMessageTemplate";
+            }
+
+            internal static class MoreOptions
+            {
+                internal static string Header => "More Options";
+                internal static string Description => "Confirm your phone number";
+                internal static string ImageName => "NSTouchBarIconViewTemplate";
+            }
+        }
+
+        static class Image
+        {
+            internal static NSImage ResendCode => NSImage.ImageNamed(String.ResendCode.ImageName);
+            internal static NSImage UsePhoneNumber => NSImage.ImageNamed(String.UsePhoneNumber.ImageName);
+            internal static NSImage MoreOptions => NSImage.ImageNamed(String.MoreOptions.ImageName);
+        }
+
+        static class Selector
+        {
+            internal static ObjCRuntime.Selector ResendCode_ => new ObjCRuntime.Selector("ResendCode:");
+            internal static ObjCRuntime.Selector UsePhoneNumber_ => new ObjCRuntime.Selector("UsePhoneNumber:");
+            internal static ObjCRuntime.Selector MoreOptions_ => new ObjCRuntime.Selector("MoreOptions:");
+        }
+    }
+}
