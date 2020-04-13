3a8acce219b5f3501ee091eb5051e0139e4df8cc
ffd <ffdelacruz@icloud.com>
2020-04-13 15:14:23 +0800

    Create String.KeyEquivalent enum

--- Statics/String.cs
+++ Statics/String.cs
@@ -4,10 +4,16 @@ namespace Balsamic
 {
     static class String
     {
+        internal enum KeyEquivalent
+        {
+            [Description("\r")]
+            Return
+        }
+
         internal enum ErrorMessage
         {
             [Description("⚠️ Not a valid email")]
-            InvalidEmail = 0
+            InvalidEmail
         }
     }
 }
