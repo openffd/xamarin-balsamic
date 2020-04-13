1c3016b29f52e6c2bc33791a4b4bddbf371e183b
ffd <ffdelacruz@icloud.com>
2020-04-13 15:26:05 +0800

    ResendCodeView: Remove unnecessary using statements

--- Views/ResendCodeView.cs
+++ Views/ResendCodeView.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Foundation;
-using AppKit;
 
 namespace Balsamic.Views
 {
@@ -10,23 +7,18 @@ namespace Balsamic.Views
     {
         #region Constructors
 
-        // Called when created from unmanaged code
         public ResendCodeView(IntPtr handle) : base(handle)
         {
             Initialize();
         }
 
-        // Called when created directly from a XIB file
         [Export("initWithCoder:")]
         public ResendCodeView(NSCoder coder) : base(coder)
         {
             Initialize();
         }
 
-        // Shared initialization code
-        void Initialize()
-        {
-        }
+        void Initialize() {}
 
         #endregion
     }
