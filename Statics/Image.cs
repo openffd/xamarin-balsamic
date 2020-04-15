160788252fab83fb7116cc376cdeb4a98b6ae1be
ffd <ffdelacruz@icloud.com>
2020-04-15 11:13:21 +0800

    Statics/Image: Add definitions for system images used

--- Statics/Image.cs
+++ Statics/Image.cs
@@ -1,8 +1,25 @@
 ﻿using AppKit;
+using System.ComponentModel;
 
 namespace Balsamic
 {
     static class Image
     {
+        static class System
+        {
+            internal enum Name
+            {
+                [Description("NSGoForwardTemplate")]
+                GoForwardTemplate,
+                [Description("NSStatusUnavailable")]
+                StatusUnavailable,
+                [Description("NSUserAccounts")]
+                UserAccounts,
+            }
+        }
+
+        internal static NSImage GoForwardTemplate => NSImage.ImageNamed(System.Name.GoForwardTemplate.String());
+        internal static NSImage StatusUnavailable => NSImage.ImageNamed(System.Name.StatusUnavailable.String());
+        internal static NSImage UserAccounts => NSImage.ImageNamed(System.Name.UserAccounts.String());
     }
 }
