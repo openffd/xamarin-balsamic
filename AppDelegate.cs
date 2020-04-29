cffc1a784ffcd105ca4a827fe71960f5d608926b
ffd <ffdelacruz@icloud.com>
2020-04-30 00:37:54 +0800

    Setup code signing, provisioning, and package signing

--- AppDelegate.cs
+++ AppDelegate.cs
@@ -1,6 +1,7 @@
 ﻿using AppKit;
 using static AppKit.NSApplicationTerminateReply;
 using Foundation;
+using Sparkle;
 
 namespace Balsamic
 {
@@ -15,13 +16,29 @@ namespace Balsamic
 
         readonly IWindowController launchWindowController = new LaunchWindowController();
         readonly IWindowController myAppsWindowController = Storyboard.MyApps.InstantiateInitialController() as Views.MyAppsWindowController;
+        readonly SUUpdater updater = new SUUpdater();
 
         public AppDelegate() {}
 
         public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender) => true;
 
+        public override void WillFinishLaunching(NSNotification notification)
+        {
+            var item = new NSMenuItem("Check for Updates...", delegate {
+                System.Console.WriteLine("Checking for updates...");
+            });
+            NSApplication.SharedApplication.MainMenu.Items[0].Submenu.InsertItem(item, 1);
+        }
+
+        void SetupCheckForUpdatesMenuItem()
+        {
+
+        }
+
         public override void DidFinishLaunching(NSNotification notification)
         {
+            //NSApplication.SharedApplication.MainMenu.Items[0].Submenu.Inse //Items[1].Action = new ObjCRuntime.Selector("Hello");
+
             myAppsWindowController.ShowWindow();
         }
         
