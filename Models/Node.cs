11c6e56dd9f4b8caf41c9875364f70d753fab45d
ffd <ffdelacruz@icloud.com>
2020-05-12 21:43:45 +0800

    Create new model Node for outline view items

---
+++ Models/Node.cs
@@ -0,0 +1,23 @@
+﻿using AppKit;
+using Foundation;
+using System.Collections.Generic;
+
+namespace Balsamic.Models
+{
+    sealed class Node : NSObject 
+    {
+        internal List<Node> Nodes = new List<Node>();
+        internal NSImage Image { get; set; }
+        internal string Title { get; set; } = string.Empty;
+        internal new string Description { get; set; } = string.Empty;
+        internal bool HasChildren => Nodes.Count > 0;
+
+        public Node() {}
+
+        public Node(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
