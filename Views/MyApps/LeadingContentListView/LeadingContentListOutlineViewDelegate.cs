519b2985f54602788352a34e9de5b12f2b4135e7
ffd <ffdelacruz@icloud.com>
2020-05-14 10:58:27 +0800

    LeadingContentListOutlineViewDelegate: Implement NSOutlineViewDelegate interface

--- Views/MyApps/LeadingContentListView/LeadingContentListOutlineViewDelegate.cs
+++ Views/MyApps/LeadingContentListView/LeadingContentListOutlineViewDelegate.cs
@@ -1,12 +1,31 @@
 ﻿using AppKit;
+using Foundation;
 
 namespace Balsamic.Views.MyApps
 {
     public class LeadingContentListOutlineViewDelegate : NSOutlineViewDelegate
     {
-        public LeadingContentListOutlineViewDelegate()
-        {
+        private readonly LeadingContentListOutlineViewDataSource dataSource;
 
+        public LeadingContentListOutlineViewDelegate(LeadingContentListOutlineViewDataSource dataSource)
+        {
+            this.dataSource = dataSource;
+        }
+
+        public override NSView GetView(NSOutlineView outlineView, NSTableColumn tableColumn, NSObject item)
+        {
+            NSTableCellView tableCellView = (NSTableCellView)outlineView.MakeView(tableColumn.Title, this);
+            if (tableCellView == null)
+            {
+                tableCellView = new NSTableCellView();
+
+            }
+            return tableCellView;
+        }
+
+        public override bool ShouldSelectItem(NSOutlineView outlineView, NSObject item)
+        {
+            return true;
         }
     }
 }
