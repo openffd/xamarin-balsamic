fb8682772e2f3762a4fdb4d4c111503bba96ad91
ffd <ffdelacruz@icloud.com>
2020-05-14 10:49:34 +0800

    Fully implement Node model

--- Models/Node.cs
+++ Models/Node.cs
@@ -6,18 +6,69 @@ namespace Balsamic.Models
 {
     sealed class Node : NSObject 
     {
-        internal List<Node> Nodes = new List<Node>();
-        internal NSImage Image { get; set; }
+        private readonly List<Node> _nodes = new List<Node>();
+
         internal string Title { get; set; } = string.Empty;
         internal new string Description { get; set; } = string.Empty;
-        internal bool HasChildren => Nodes.Count > 0;
+        internal NSImage Image { get; set; }
+        internal string Tag { get; set; } = string.Empty;
 
         public Node() {}
 
-        public Node(string title, string description)
+        public Node(string title, string description, NSImage image, string tag, SelectionDelegate selectionDelegate)
         {
             Title = title;
             Description = description;
+            Image = image;
+            Tag = tag;
+            Delegate = selectionDelegate;
+        }
+
+        #region Indexing
+
+        internal Node this[int index]
+        {
+            get => _nodes[index];
+            set => _nodes[index] = value;
+        }
+
+        internal int Count => _nodes.Count;
+
+        internal bool HasChildren => Count > 0;
+
+        #endregion
+
+        internal void Add(Node childNode)
+        {
+            _nodes.Add(childNode);
+        }
+
+        internal void Insert(int index, Node childNode)
+        {
+            _nodes.Insert(index, childNode);
+        }
+
+        internal void Remove(Node childNode)
+        {
+            _nodes.Remove(childNode);
+        }
+
+        internal void RemoveAt(int index)
+        {
+            _nodes.RemoveAt(index);
+        }
+
+        internal void Clear()
+        {
+            _nodes.Clear();
+        }
+
+        internal delegate void SelectionDelegate();
+        internal event SelectionDelegate Delegate;
+
+        internal void RaiseSelectedEvent()
+        {
+            Delegate?.Invoke();
         }
     }
 }
