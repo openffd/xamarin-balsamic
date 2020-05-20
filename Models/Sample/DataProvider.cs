7ea223fc1192ba5262e9424f702f199706700acd
ffd <ffdelacruz@icloud.com>
2020-05-21 00:11:42 +0800

    Create an object that initializes sample data

---
+++ Models/Sample/DataProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using static Foundation.NSBundle;
+
+namespace Balsamic.Models.Samples
+{
+    sealed class DataProvider
+    {
+        public Account Account { get; set; }
+
+        public DataProvider()
+        {
+            var path = MainBundle.PathForResource("Data/Account/account", "json");
+            Account = JsonConvert.DeserializeObject<Account>(File.ReadAllText(path));
+        }
+
+
+    }
+
+    class Account
+    {
+        [JsonProperty(PropertyName = "apple-id-key")]
+        public string AppleIdKey { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "obf-apple-id")]
+        public string ObfuscatedAppleId { get; set; }
+
+        [JsonProperty(PropertyName = "team-id-key")]
+        public string TeamIdKey { get; set; }
+
+        [JsonProperty(PropertyName = "team-name")]
+        public string TeamName { get; set; }
+
+        [JsonProperty(PropertyName = "version")]
+        public string Version { get; set; }
+    }
+}
