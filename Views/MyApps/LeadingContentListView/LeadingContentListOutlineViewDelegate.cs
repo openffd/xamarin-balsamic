ed9662bf460734d60443b5fd8137a05405cfbbf2
ffd <ffdelacruz@icloud.com>
2020-05-20 20:07:58 +0800

    LeadingContentListOutlineViewDelegate: Implement other delegate interface methods

--- Views/MyApps/LeadingContentListView/LeadingContentListOutlineViewDelegate.cs
+++ Views/MyApps/LeadingContentListView/LeadingContentListOutlineViewDelegate.cs
@@ -1,31 +1,41 @@
-﻿using AppKit;
+﻿using System;
+using AppKit;
+using Balsamic.Models;
 using Foundation;
 
 namespace Balsamic.Views.MyApps
 {
     public class LeadingContentListOutlineViewDelegate : NSOutlineViewDelegate
     {
-        private readonly LeadingContentListOutlineViewDataSource dataSource;
+        public LeadingContentListOutlineViewDelegate() {}
 
-        public LeadingContentListOutlineViewDelegate(LeadingContentListOutlineViewDataSource dataSource)
+        public override NSCell GetCell(NSOutlineView outlineView, NSTableColumn tableColumn, NSObject item)
         {
-            this.dataSource = dataSource;
+            nint row = outlineView.RowForItem(item); 
+            return tableColumn.DataCellForRow(row);
         }
 
         public override NSView GetView(NSOutlineView outlineView, NSTableColumn tableColumn, NSObject item)
         {
-            NSTableCellView tableCellView = (NSTableCellView)outlineView.MakeView(tableColumn.Title, this);
+            NSTableCellView tableCellView = (NSTableCellView)outlineView.MakeView("DataCell", this);
             if (tableCellView == null)
             {
                 tableCellView = new NSTableCellView();
-
             }
+            tableCellView.TextField.StringValue = (item as Node).Title;
             return tableCellView;
         }
 
+        public override bool IsGroupItem(NSOutlineView outlineView, NSObject item) => (item as Node).HasChildren;
+
         public override bool ShouldSelectItem(NSOutlineView outlineView, NSObject item)
         {
-            return true;
+            return outlineView.GetParent(item) != null;
+        }
+
+        public override bool ShouldEditTableColumn(NSOutlineView outlineView, NSTableColumn tableColumn, NSObject item)
+        {
+            return false;
         }
     }
 }
