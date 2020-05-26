947bdd9d4de8731be4d11255b5fd16383423ce79
ffd <ffdelacruz@icloud.com>
2020-05-27 00:45:30 +0800

    DataProvider: Sample data provider class initial implementation

--- Models/Sample/DataProvider.cs
+++ Models/Sample/DataProvider.cs
@@ -1,112 +1,40 @@
-﻿using Foundation;
-using System.IO;
-using Newtonsoft.Json;
-using static Balsamic.String.FileExtension;
+﻿using static Balsamic.String.FileExtension;
 using static Foundation.NSBundle;
 using static Newtonsoft.Json.JsonConvert;
 using System.Collections.Generic;
+using System.IO;
 
-namespace Balsamic.Models.Samples
+namespace Balsamic.Models.Sample
 {
-    class Version : NSObject
-    {
-        public static string ResourcePath = "Data/Version/versions";
-
-        [JsonProperty(PropertyName = "app-id")]
-        public string AppId { get; set; }
-
-        [JsonProperty(PropertyName = "copyright")]
-        public string Copyright { get; set; }
-
-        [JsonProperty(PropertyName = "id")]
-        public string Id { get; set; }
-
-        [JsonProperty(PropertyName = "metadata")]
-        public List<Metadata> Metadata { get; set;}
-
-        [JsonProperty(PropertyName = "origin")]
-        public string Origin { get; set; }
-
-        [JsonProperty(PropertyName = "platform")]
-        public string Platform { get; set; }
-
-        [JsonProperty(PropertyName = "review")]
-        public Review Review { get; set; }
-
-        [JsonProperty(PropertyName = "state")]
-        public string State { get; set; }
-
-        [JsonProperty(PropertyName = "store-id")]
-        public string StoreId { get; set; }
-
-        [JsonProperty(PropertyName = "version")]
-        public string VersionString { get; set; }
-    }
-
-    public class Review: NSObject
-    {
-        [JsonProperty(PropertyName = "email")]
-        public string Email { get; set; }
-
-        [JsonProperty(PropertyName = "first-name")]
-        public string FirstName { get; set; }
-
-        [JsonProperty(PropertyName = "last-name")]
-        public string LastName { get; set; }
-
-        [JsonProperty(PropertyName = "needs-credentials")]
-        public bool NeedsCredentials { get; set; }
-
-        [JsonProperty(PropertyName = "phone")]
-        public string Phone { get; set; }
-    }
-
-    public class Metadata : NSObject
-    {
-        [JsonProperty(PropertyName = "app-store-name")]
-        public string AppStoreName { get; set; }
-
-        [JsonProperty(PropertyName = "app-store-subtitle")]
-        public string AppStoreSubtitle { get; set; }
-
-        [JsonProperty(PropertyName = "description")]
-        public new string Description { get; set; }
-
-        [JsonProperty(PropertyName = "id")]
-        public string Id { get; set; }
-
-        [JsonProperty(PropertyName = "keywords")]
-        public string Keywords { get; set; }
-
-        [JsonProperty(PropertyName = "lang")]
-        public string Lang { get; set; }
-
-        [JsonProperty(PropertyName = "store-id")]
-        public string StoreId { get; set; }
-
-        [JsonProperty(PropertyName = "support-url")]
-        public string SupportUrl { get; set; }
-
-        [JsonProperty(PropertyName = "version-id")]
-        public string VersionId { get; set; }
-    }
-
     sealed class DataProvider
     {
-        public Account Account { get; set; }
-        public List<Application> Applications { get; set; }
-        public List<Version> Versions { get; set; }
+        public AppleDevAccount AppleDevAccount { get; set; }
+        public List<ApplicationDetail> ApplicationDetails { get; set; }
+        public List<ApplicationVersion> ApplicationVersions { get; set; }
 
         public DataProvider()
         {
-            var accountJsonPath = MainBundle.PathForResource(Account.ResourcePath, Json.String());
-            Account = DeserializeObject<Account>(File.ReadAllText(accountJsonPath));
+            SetupAppleDevAccount();
+            SetupApplicationDetails();
+            SetupApplicationVersions();
+        }
 
-            var applicationsJsonPath = MainBundle.PathForResource(Application.ResourcePath, Json.String());
-            Applications = DeserializeObject<List<Application>>(File.ReadAllText(applicationsJsonPath));
+        private void SetupAppleDevAccount()
+        {
+            var path = MainBundle.PathForResource(AppleDevAccount.ResourcePath, Json.String());
+            AppleDevAccount = DeserializeObject<AppleDevAccount>(File.ReadAllText(path));
+        }
 
-            var versionsJsonPath = MainBundle.PathForResource(Version.ResourcePath, Json.String());
-            Versions = DeserializeObject<List<Version>>(File.ReadAllText(versionsJsonPath));
+        private void SetupApplicationDetails()
+        {
+            var path = MainBundle.PathForResource(ApplicationDetail.ResourcePath, Json.String());
+            ApplicationDetails = DeserializeObject<List<ApplicationDetail>>(File.ReadAllText(path));
+        }
+
+        private void SetupApplicationVersions()
+        {
+            var path = MainBundle.PathForResource(ApplicationVersion.ResourcePath, Json.String());
+            ApplicationVersions = DeserializeObject<List<ApplicationVersion>>(File.ReadAllText(path));
         }
     }
 }
