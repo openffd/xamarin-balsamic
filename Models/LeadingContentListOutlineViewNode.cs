b51894cb6a7b188e3e476b5af3fd185a04769713
ffd <ffdelacruz@icloud.com>
2020-05-27 01:34:24 +0800

    Create new model class LeadingContentListOutlineViewNode

---
+++ Models/LeadingContentListOutlineViewNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using static Balsamic.Models.LeadingContentListOutlineViewNodeType;
+
+namespace Balsamic.Models
+{
+    internal enum LeadingContentListOutlineViewNodeType
+    {
+        Account,
+        Application,
+        Version,
+        Separator,
+        Unknown
+    }
+
+    sealed class LeadingContentListOutlineViewNode : Node
+    {
+        internal LeadingContentListOutlineViewNodeType NodeType { get; set; } = Unknown;
+
+        internal List<Node> Children => _nodes;
+
+        internal bool IsAccount     => NodeType == Account;
+        internal bool IsApplication => NodeType == Application;
+        internal bool IsVersion     => NodeType == Version;
+        internal bool IsSeparator   => NodeType == Separator;
+
+        internal bool Leaf => IsVersion || IsSeparator;
+    }
+}
