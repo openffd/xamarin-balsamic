8073c3f2c624afb1951d1f25826121093cb2c0c5
ffd <ffdelacruz@icloud.com>
2020-06-01 06:51:20 +0800

    Statics/Enums: Create new file that contains app-wide enums

---
+++ Statics/Enums.cs
@@ -0,0 +1,30 @@
+﻿using AppKit;
+using System;
+using System.ComponentModel;
+using static AppKit.NSColor;
+using static Balsamic.Enums.ApplicationVersion.Status;
+
+namespace Balsamic.Enums
+{
+    static class ApplicationVersion
+    {
+        internal enum Status
+        {
+            [Description("Developer Removed from Sale")]    DeveloperRemovedFromSale,
+            [Description("Prepare for Submission")]         PrepareForSubmission,
+            [Description("Ready for Sale")]                 ReadyForSale,
+            [Description("Unknown")]                        Unknown,
+        }
+
+        internal static NSColor StatusColor(this Status status)
+        {
+            return status switch
+            {
+                DeveloperRemovedFromSale    => Red,
+                PrepareForSubmission        => Yellow,
+                ReadyForSale                => Green,
+                _                           => White,
+            };
+        }
+	}
+}
