c4df508099d84dfbadf11704f8278ff655bafdc0
ffd <ffdelacruz@icloud.com>
2020-06-03 03:36:32 +0800

    Create NSView static class containing extension methods

---
+++ Statics/NSView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Balsamic
+{
+    static class NSView
+    {
+        internal static void Rounded(this AppKit.NSView view, double cornerRadius = 4)
+        {
+            view.WantsLayer = true;
+            view.CanDrawSubviewsIntoLayer = true;
+            view.Layer.MasksToBounds = true;
+            view.Layer.CornerRadius = (nfloat)cornerRadius;
+        }
+    }
+}
