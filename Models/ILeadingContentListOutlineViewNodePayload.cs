809f086100b10975a7945c872b723eef7c2efe66
ffd <ffdelacruz@icloud.com>
2020-06-04 11:07:31 +0800

    Create new interface file ILeadingContentListOutlineViewNodePayload

---
+++ Models/ILeadingContentListOutlineViewNodePayload.cs
@@ -0,0 +1,22 @@
+﻿using AppKit;
+using Foundation;
+using static Balsamic.Models.LeadingContentListOutlineViewNodeType;
+
+namespace Balsamic.Models
+{
+    internal interface ILeadingContentListOutlineViewNodePayload
+    {
+        LeadingContentListOutlineViewNodeType NodeType { get; }
+        NSImage Image { get; }
+        string Title { get; }
+        string Subtitle { get; }
+    }
+
+    sealed class LeadingContentListOutlineViewSeparator : NSObject, ILeadingContentListOutlineViewNodePayload
+    {
+        public LeadingContentListOutlineViewNodeType NodeType => Separator;
+        public NSImage Image => null;
+        public string Title => string.Empty;
+        public string Subtitle => string.Empty;
+    }
+}
