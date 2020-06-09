2f24422a12c728e2709f9919c84ed0aa433e1319
ffd <ffdelacruz@icloud.com>
2020-06-09 21:59:11 +0800

    Create partial class LeadingContentListViewController.Nib

---
+++ Views/MyApps/LeadingContentListView/LeadingContentListViewController.Nib.cs
@@ -0,0 +1,12 @@
+﻿using AppKit;
+
+namespace Balsamic.Views.MyApps
+{
+    internal sealed partial class LeadingContentListViewController
+    {
+        private static class Nib
+        {
+            internal static NSNib ApplicationDetailTableCellView => new NSNib("ApplicationDetailTableCellView", null);
+        }
+    }
+}
