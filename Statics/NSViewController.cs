52c91f4b0a28e19aa02f625c314258d07eb4c866
ffd <ffdelacruz@icloud.com>
2020-06-09 21:32:53 +0800

    Create NSViewController extension methods

---
+++ Statics/NSViewController.cs
@@ -0,0 +1,22 @@
+﻿using AppKit;
+using System.Linq;
+
+namespace Balsamic
+{
+    internal static class ViewController
+    {
+        internal static bool HasChildViewController(this NSViewController viewController)
+        {
+            return viewController.ChildViewControllers.Count() > 0;
+        }
+
+        internal static void RemoveFirstChildViewController(this NSViewController viewController)
+        {
+            if (!viewController.HasChildViewController())
+                return;
+
+            viewController.RemoveChildViewController(0);
+            viewController.View.Subviews.First().RemoveFromSuperview();
+        }
+    }
+}
