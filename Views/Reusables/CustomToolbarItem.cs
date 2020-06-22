d042b8eaa10dea11c77e2f23bb315585caaff8b5
ffd <ffdelacruz@icloud.com>
2020-06-22 16:53:25 +0800

    Create new reusable view class CustomToolbarItem

---
+++ Views/Reusables/CustomToolbarItem.cs
@@ -0,0 +1,24 @@
+﻿using AppKit;
+using CoreGraphics;
+using System;
+
+namespace Balsamic.Views
+{
+    [Foundation.Register("CustomToolbarItem")]
+    internal sealed class CustomToolbarItem : NSToolbarItem
+    {
+        public nfloat CustomHeight { get; set; }
+        public nfloat CustomWidth { get; set; }
+
+        public override CGSize MinSize
+        {
+            get => new CGSize(CustomWidth, CustomHeight);
+            set => CustomWidth = value.Width;
+        }
+
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+        }
+    }
+}
